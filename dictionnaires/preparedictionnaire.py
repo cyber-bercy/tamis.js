5e764fd64c0adf0c7f353314ab9e827f3bd5113e
Jean-Philippe Papillon <open-source@aglossa.net>
2019-09-24 20:15:51 +0200

    dict: mix Richelieu et specifique

--- dictionnaires/preparedictionnaire.py
+++ dictionnaires/preparedictionnaire.py
@@ -5,10 +5,10 @@ import fileinput
 vocabulaire = {}
 
 for ligne in fileinput.input():
-    mot = ligne.strip("\n ")
+    mot = ligne.strip("\n ").lower()
     if mot[0:2] <> "//" :
         vocabulaire[mot] = len(mot)
 
-for mot, longeur in vocabulaire.items():
+for mot, longueur in vocabulaire.items():
     if longueur >= 4 :
         print ( "\"{0}\": {1},".format(mot, longueur) )
